88e3bcf23828ff4b13c411aca8b75933c4addc79
Vitaliy D <vi7alya@gmail.com>
2022-05-22 13:04:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+ESP8266 Client for the Train Pusher
+===================================
+
+ESP8266 FreeRTOS based client for the [Train Pusher](https://github.com/vi7/train-pusher)
+
+Project configuration
+---------------------
+
+Install ESP toolchain and SDK:
+```bash
+./setup.sh
+```
+
+Then start project configuration utility by running `make menuconfig`. In the menu, navigate to `Serial flasher config` > `Default serial port` to configure the serial port, where project will be loaded to. Confirm selection by pressing enter, save configuration by selecting `< Save >` and then exit application by selecting `< Exit >`.
+
+
+Development
+-----------
+
+Cloning the project and submodules:
+```bash
+git clone --recurse-submodules https://github.com/vi7/iot-controller.git
+```
+
+Updating submodules:
+```bash
+git submodule update --init --recursive
+# OR if changes already fetched and visible in the status:
+git submodule update --remote
+```
+
+Working on a submodule:
+```bash
+# update local submodule branch by rebasing:
+git submodule update --remote --rebase
+```
+
+Activate Python virtualenv with `source venv/bin/activate`
+
+Compile project: `make all`
+
+Compile and flash: `make flash`
+
+Viewing serial output: `make monitor`
+
+Serial monitor hotkeys: Quit: Ctrl+] | Menu: Ctrl+T | Help: Ctrl+T followed by Ctrl+H
+
+Build just the app: `make app`
+
+Flash just the app: `make app-flash` - will automatically rebuild the app if it needs it
+
+Speed up build with compiling multiple files in parallel: `make -jN app-flash` - where N is the number of parallel make processes to run (generally N should be equal to or one more than the number of CPU cores in your system.)
+
+Erase the entire flash: `make erase_flash`
+
+Make targets could be combined in one run
---
+++ main/train_pusher_client_main.c
@@ -0,0 +1,58 @@
+static void http_rest_with_url(void)
+{
+    char local_response_buffer[MAX_HTTP_OUTPUT_BUFFER] = {0};
+    /**
+     * NOTE: All the configuration parameters for http_client must be spefied either in URL or as host and path parameters.
+     * If host and path parameters are not set, query parameter will be ignored. In such cases,
+     * query parameter should be specified in URL.
+     *
+     * If URL as well as host and path parameters are specified, values of host and path will be considered.
+     */
+    esp_http_client_config_t config = {
+        .host = "httpbin.org",
+        .path = "/get",
+        .query = "esp",
+        .event_handler = _http_event_handler,
+        .user_data = local_response_buffer,        // Pass address of local buffer to get response
+    };
+    esp_http_client_handle_t client = esp_http_client_init(&config);
+
+    // GET
+    esp_err_t err = esp_http_client_perform(client);
+    if (err == ESP_OK) {
+        ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %d",
+                esp_http_client_get_status_code(client),
+                esp_http_client_get_content_length(client));
+    } else {
+        ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
+    }
+    ESP_LOG_BUFFER_HEX(TAG, local_response_buffer, strlen(local_response_buffer));
+}
+
+static void http_test_task(void *pvParameters)
+{
+    http_rest_with_url();
+    ESP_LOGI(TAG, "Finish http example");
+    vTaskDelete(NULL);
+}
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+      ESP_ERROR_CHECK(nvs_flash_erase());
+      ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
+     * Read "Establishing Wi-Fi or Ethernet Connection" section in
+     * examples/protocols/README.md for more information about this function.
+     */
+    ESP_ERROR_CHECK(example_connect());
+    ESP_LOGI(TAG, "Connected to AP, begin http example");
+
+    xTaskCreate(&http_test_task, "http_test_task", 8192, NULL, 5, NULL);
+}
